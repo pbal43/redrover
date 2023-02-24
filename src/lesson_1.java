05698d520d8f2e7f22e30d00aaae99cb32186fed
pbal43 <pbal43@gmail.com>
2023-02-24 10:12:30 +0300

    first_hw

---
+++ src/lesson_1.java
@@ -0,0 +1,10 @@
+public class lesson_1 {
+    public static void main(String[] args) {
+//        super-program
+        System.out.println("Hello world!");
+        System.out.print(123);
+        System.out.println("Hello world"+"!");
+        System.out.println("Hello\t wor\nld!");
+        System.out.println("Hello" + 123);
+    }
+}
