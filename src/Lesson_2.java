58fe8a770473c8105330b094cea1e683fe38b991
pbal43 <pbal43@gmail.com>
2023-02-24 20:28:28 +0300

    lesson_2 done

--- src/Lesson_2.java
+++ src/Lesson_2.java
@@ -1,8 +1,25 @@
 public class Lesson_2 {
     public static void main(String[] args) {
+
         int apple = 6;
         int people = 2;
         int result = apple / people;
         System.out.println(result);
+
+        double Apple = 7;
+        double People = 2;
+        double Result = Apple / People;
+        System.out.println(Result);
+
+
+        String str = "";
+
+        boolean b_1 = true;
+        boolean b_2 = false;
+
+        char symbol = 'a'; // always 1 symbol in char but digit (number of symbol) - code for 'a' is 97
+        int some = 1;
+        System.out.println(symbol + some);
+
     }
 }
