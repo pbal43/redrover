6174f89f3d1cdf9f3cfefcb3f2d3b408315ce724
pbal43 <pbal43@gmail.com>
2023-03-01 08:58:23 +0300

    start lesson 3

--- src/Lesson_3.java
+++ src/Lesson_3.java
@@ -46,5 +46,20 @@ public class Lesson_3 {
         System.out.println(i);
         System.out.println(q);
 
+        System.out.println( " true && false = " + ( true && false));
+        System.out.println( " true && true  = " + ( true && true));
+        System.out.println( "false && true  = " + (false && true));
+        System.out.println( "false && false = " + (false && false));
+
+        System.out.println("=========================================");
+        System.out.println( "true  || true = " +  (true  || true));
+        System.out.println( "true  || false = " + (true  || false));
+        System.out.println( "false || true  = " + (false || true));
+        System.out.println( "false || false = " + (false || false));
+
+        System.out.println("=========================================");
+        System.out.println( "!false = " + (!false));
+        System.out.println( "!true = "  + (!true));
+
     }
 }
