8d98c406f71c6e4e04dbd3d5a7dc15560e6fad4e
pbal43 <pbal43@gmail.com>
2023-03-03 10:45:09 +0300

    Hw 4

---
+++ src/hw/Hw_4.java
@@ -0,0 +1,46 @@
+package hw;
+
+public class Hw_4 {
+    public static void main(String[] args) {
+//        1
+
+        for (int i = 0; i < 16; i += 1) {
+            System.out.println(i);
+        }
+
+//        2
+
+        for (int i = 1; i < 10000; i += 1) {
+            System.out.println("Степень числа 5 в итерации = " + i);
+            int res = (int) Math.pow(5, i);
+            System.out.println("Результат возведения числа 5 в степень = " + res);
+        }
+
+//        или
+
+        int goal = 5;
+        for (int i = 1; i < 10000; i += 1) {
+            System.out.println("Степень числа 5 в итерации = " + i);
+            if (i > 1) {
+                goal *= 5;
+            }
+            System.out.println("Результат возведения числа 5 в степень = " + goal);
+        }
+
+//        3
+
+//        3.1
+
+        for (int i = 40; i < 61; i = i + 1) {
+            if (i % 4 == 0) {
+                System.out.println("3 задача для 1 варианта" + i);
+            }
+        }
+
+//        3. 2
+
+        for (int i = 40; i < 61; i = i + 4) {
+            System.out.println("3 задача для 2 варианта" + i);
+        }
+    }
+}
