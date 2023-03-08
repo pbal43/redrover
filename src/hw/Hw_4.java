b60e46748c88d15fbb01f38c572be3f5a9e2f152
pbal43 <pbal43@gmail.com>
2023-03-08 04:34:05 +0300

    Hw 4

--- src/hw/Hw_4.java
+++ src/hw/Hw_4.java
@@ -27,20 +27,26 @@ public class Hw_4 {
             System.out.println("Результат возведения числа 5 в степень = " + goal);
         }
 
+//        или другое понимание задачи: результат возведения в степень дб меньше 10000
+
+        for (int i = 1; i < 10000; i *= 5) {
+            System.out.println(i);
+        }
+
 //        3
 
 //        3.1
 
         for (int i = 40; i < 61; i = i + 1) {
             if (i % 4 == 0) {
-                System.out.println("3 задача для 1 варианта" + i);
+                System.out.println("3 задача для 1 варианта " + i);
             }
         }
 
 //        3. 2
 
         for (int i = 40; i < 61; i = i + 4) {
-            System.out.println("3 задача для 2 варианта" + i);
+            System.out.println("3 задача для 2 варианта " + i);
         }
     }
 }
