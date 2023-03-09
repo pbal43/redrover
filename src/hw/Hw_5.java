8a1a380f360c76f1279de3f1b384609382a0cf0d
pbal43 <pbal43@gmail.com>
2023-03-09 05:50:56 +0300

    Hw 5

--- src/hw/Hw_5.java
+++ src/hw/Hw_5.java
@@ -3,5 +3,21 @@ package hw;
 public class Hw_5 {
     public static void main(String[] args) {
 
+//        1
+
+        int rows = 10;
+        int columns = 10;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+            columns--;
+        }
+
+//        2
+
+
+
     }
 }
