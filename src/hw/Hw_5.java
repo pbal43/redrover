f638f799b718d705586468629e3a811a27b8de53
pbal43 <pbal43@gmail.com>
2023-03-09 06:52:32 +0300

    Hw 5 done

--- src/hw/Hw_5.java
+++ src/hw/Hw_5.java
@@ -5,52 +5,51 @@ public class Hw_5 {
 
 //        1
 
-//        System.out.println("==================1=================");
+        System.out.println("==================1=================");
 
-//        int rows_1 = 10;
-//        int columns_1 = 10;
-//        for (int i = 0; i < rows_1; i++) {
-//            for (int j = 0; j < columns_1; j++) {
-//                System.out.print(j + " ");
-//            }
-//            System.out.println();
-//            columns_1--;
-//        }
-//
+        int rows_1 = 10;
+        int columns_1 = 10;
+        for (int i = 0; i < rows_1; i++) {
+            for (int j = 0; j < columns_1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+            columns_1--;
+        }
 
 //        2
 
-//        System.out.println("==================2=================");
+        System.out.println("==================2=================");
 
-//        String space = " ";
-//        int spaces = 2;
-//        int rows = 10;
-//        int columns = 10;
-//        for (int i = 0; i < rows; i++) {
-//            for (int j = 0; j < columns; j++) {
-//                System.out.print(j + space);
-//            }
-//            System.out.print("\n" + space.repeat(spaces));
-//            spaces+=2;
-//            columns--;
-//        }
+        String space_2 = " ";
+        int spaces_2 = 2;
+        int rows_2 = 10;
+        int columns_2 = 10;
+        for (int i = 0; i < rows_2; i++) {
+            for (int j = 0; j < columns_2; j++) {
+                System.out.print(j + space_2);
+            }
+            System.out.print("\n" + space_2.repeat(spaces_2));
+            spaces_2+=2;
+            columns_2--;
+        }
 
 //        3
+        System.out.println();
+        System.out.println("=================3==================");
 
-//        System.out.println("=================3==================");
-
-        String space = " ";
-        int spaces = 2;
-        int rows = 10;
+        String space_3 = " ";
+        int spaces_3 = 2;
+        int rows_3 = 10;
         int min_columns = -9;
-        int columns = 10;
-        for (int i = 0; i < rows; i++) {
-            for (int j = min_columns; j < columns; j++) {
+        int columns_3 = 10;
+        for (int i = 0; i < rows_3; i++) {
+            for (int j = min_columns; j < columns_3; j++) {
                 System.out.print(Math.abs(j) + " ");
             }
-            System.out.print("\n" + space.repeat(spaces));
-            spaces+=2;
-            columns--;
+            System.out.print("\n" + space_3.repeat(spaces_3));
+            spaces_3+=2;
+            columns_3--;
             min_columns++;
         }
 
