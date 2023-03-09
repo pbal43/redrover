d1b3dbf8ba849f6caa0c18fb8dca63a0dbb1c374
pbal43 <pbal43@gmail.com>
2023-03-09 06:26:01 +0300

    Hw 5

--- src/hw/Hw_5.java
+++ src/hw/Hw_5.java
@@ -5,6 +5,8 @@ public class Hw_5 {
 
 //        1
 
+//        System.out.println("==================1=================");
+
 //        int rows_1 = 10;
 //        int columns_1 = 10;
 //        for (int i = 0; i < rows_1; i++) {
@@ -15,35 +17,40 @@ public class Hw_5 {
 //            columns_1--;
 //        }
 //
-//        System.out.println("===================================");
 
 //        2
 
-        String space = " ";
-        int spaces = 2;
-        int rows = 10;
-        int columns = 10;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                System.out.print(j + space);
-            }
-            System.out.print("\n" + space.repeat(spaces));
-            spaces+=2;
-            columns--;
-        }
+//        System.out.println("==================2=================");
 
 //        String space = " ";
-//        int spaces = 1;
+//        int spaces = 2;
 //        int rows = 10;
 //        int columns = 10;
 //        for (int i = 0; i < rows; i++) {
 //            for (int j = 0; j < columns; j++) {
-//                System.out.print(j + " ");
+//                System.out.print(j + space);
 //            }
 //            System.out.print("\n" + space.repeat(spaces));
-//            spaces++;
+//            spaces+=2;
 //            columns--;
 //        }
+
+//        3
+
+//        System.out.println("=================3==================");
+
+        String space = " ";
+        int spaces = 1;
+        int rows = 10;
+        int columns = 10;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.print("\n" + space.repeat(spaces));
+            spaces++;
+            columns--;
+        }
 
     }
 }
