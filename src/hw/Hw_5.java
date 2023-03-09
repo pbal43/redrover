8e02ec78e8d42b50abea1001ac7780f8b0fa0240
pbal43 <pbal43@gmail.com>
2023-03-09 06:16:12 +0300

    Hw 5

--- src/hw/Hw_5.java
+++ src/hw/Hw_5.java
@@ -5,19 +5,45 @@ public class Hw_5 {
 
 //        1
 
+//        int rows_1 = 10;
+//        int columns_1 = 10;
+//        for (int i = 0; i < rows_1; i++) {
+//            for (int j = 0; j < columns_1; j++) {
+//                System.out.print(j + " ");
+//            }
+//            System.out.println();
+//            columns_1--;
+//        }
+//
+//        System.out.println("===================================");
+
+//        2
+
+        String space = " ";
+        int spaces = 2;
         int rows = 10;
         int columns = 10;
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                System.out.print(j + " ");
+                System.out.print(j + space);
             }
-            System.out.println();
+            System.out.print("\n" + space.repeat(spaces));
+            spaces+=2;
             columns--;
         }
 
-//        2
-
-
+//        String space = " ";
+//        int spaces = 1;
+//        int rows = 10;
+//        int columns = 10;
+//        for (int i = 0; i < rows; i++) {
+//            for (int j = 0; j < columns; j++) {
+//                System.out.print(j + " ");
+//            }
+//            System.out.print("\n" + space.repeat(spaces));
+//            spaces++;
+//            columns--;
+//        }
 
     }
 }
