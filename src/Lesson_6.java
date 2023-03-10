919ec4f0fc808fcf4e3a60805366512c5ee2c895
pbal43 <pbal43@gmail.com>
2023-03-10 08:02:02 +0300

    Lesson 6

--- src/Lesson_6.java
+++ src/Lesson_6.java
@@ -122,5 +122,81 @@ public class Lesson_6 {
             System.out.println("a_2 == b_2");
         }
 
+//        Методы строк
+
+        String str_methods = "Sergey";
+        String str_methods_2 = str_methods.toLowerCase(); // метод приводящий к lower case, не меняющий исходную строку
+        // (функция, вызывающаяся от объекта Строки)
+        System.out.println("str_methods.toLowerCase "+ str_methods_2);
+        System.out.println("str_methods " + str_methods);
+
+        // но строку можно перезаписать:
+        String str_methods_3 = "Sergey";
+        str_methods_3 = str_methods_3.toLowerCase(); // перезаписал в эту же переменную
+        System.out.println("str_methods_3.toLowerCase "+ str_methods_3);
+
+        String str_methods_4 = "Sergey";
+        str_methods_4 = str_methods_4.replace('e', 'a'); // перезаписал в эту же переменную
+        // с заменой чаров
+        System.out.println("str_methods_4.toLowerCase "+ str_methods_4);
+
+        String str_methods_5 = "Sergey is a programmer";
+        str_methods_5 = str_methods_5.replace("programmer", "good guy"); // перезаписал в эту же переменную
+        // с заменой подстроки
+        System.out.println("str_methods_5.toLowerCase "+ str_methods_5);
+
+//        Еще поработаем с массивами
+
+        int[] arr_n = {10, 5, 3, 8, 2, 6};
+        int min_n = arr_n[0];
+        int[] arr_n_0 = {};
+        int min_n_0 = Integer.MAX_VALUE; // если будет макс значение integer, то при пустом массиве перезапишем
+        // при поиске мин значения (нельзя взять arr_n_0[0] - будет ошибка)
+        int max_n_0 = Integer.MIN_VALUE;
+        int max_n = Integer.MIN_VALUE;
+        for (int i = 0; i < arr_n.length; i++) {
+            if (max_n < arr_n[i]) {
+                max_n = arr_n[i];
+            }
+        }
+        System.out.println("max_n " + max_n);
+
+
+        Integer ee = 10;
+        System.out.println("ee " + ee.doubleValue());
+
+
+//        Еще про строки
+
+        String str_m = "Sergey is a programmer";
+        for (int i = 0; i < str_m.length(); i++) {
+            System.out.println(str_m.charAt(i));
+        }
+
+//
+
+        int[] arr_k_1 = {1, 2, 3};
+        int[] arr_k_2 = arr_k_1;
+        int[] arr_k_3 = {1, 2, 3};
+        System.out.println(arr_k_1 == arr_k_2); // будет True - один объект
+        System.out.println(arr_k_1 == arr_k_3); // будет False - разные объекты, поэтому используем
+//        Эквивалентность (как для массивов, так и для строк...)
+
+        System.out.println("equals arr " + Arrays.equals(arr_k_1, arr_k_3)); // будет true, использована эквивалентность
+        // для массивов с использоанием значений
+
+//        но для Int, так как значения -128...127 - уже сохранены в объекте java по стандарту:
+
+        Integer a_k_1 = 10;
+        Integer b_k_1 = 10;
+        System.out.println(a_k_1 == b_k_1); // тут будет True,  так как и там и там хранится 1 объект
+        // (автоматом создается заранее)
+
+        Integer a_k_2 = 1000;
+        Integer b_k_2 = 1000;
+        System.out.println(a_k_2 == b_k_2); // тут будет False, объекты разные (вне диапазона сохраненных)
+
+//        ДЛЯ СРАВНЕНИЯ ЗНАЧЕНИЙ ОБЪЕКТА НУЖНО ИСПОЛЬЗОВАТЬ EQUAL, == - только для сравнения самих объектов
+
     }
 }
