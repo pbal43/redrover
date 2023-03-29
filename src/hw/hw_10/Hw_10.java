73e938a4045e31fd8cb0f00697eeb9ccec557e32
pbal43 <pbal43@gmail.com>
2023-03-29 08:11:11 +0300

    HW_10.2 in progress

--- src/hw/hw_10/Hw_10.java
+++ src/hw/hw_10/Hw_10.java
@@ -6,7 +6,7 @@ public class Hw_10 {
 
     public static void main(String[] args) {
 
-
+        System.out.println(TrafficLights.updateLight("yellow"));
 
     }
 
@@ -16,6 +16,19 @@ public class Hw_10 {
             String[] arr = name.split(" ");
             String new_string = (arr[0].toUpperCase().charAt(0)) + "." + (arr[1].toUpperCase().charAt(0));
             return new_string;
+        }
+    }
+
+    public class TrafficLights {
+
+        public static String updateLight(String current) {
+            String[] lights = {"green", "yellow", "red"};
+            for (int i = 0; i < lights.length; i++) {
+                if (current.equals(lights[i]) && !current.equals("red")) {
+                    return lights[i+1];
+                }
+            }
+            return "green";
         }
     }
 
